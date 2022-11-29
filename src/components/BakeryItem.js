a7619f234e6ce4f97990ff828d0c3923f32b2d29
slowrabbit216 <114823623+slowrabbit216@users.noreply.github.com>
2022-11-29 17:45:46 -0500

    display calories and price

--- src/components/BakeryItem.js
+++ src/components/BakeryItem.js
@@ -3,9 +3,10 @@
 export default function BakeryItem({ item, index, addToCart }) {
   return (
     <div class="SingleItem">
-      <h2>
-        {index} {item.name}
-      </h2>
+      <h2>{item.name}</h2>
+      <p>{"Calories :" + item.calories}</p>
+      <p>{"Price :" + item.price}</p>
+
       <img src={item.image} alt="Pastries" />
       <button onClick={() => addToCart(index, item)}>add to favorites</button>
     </div>
